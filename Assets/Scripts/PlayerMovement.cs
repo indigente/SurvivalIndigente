488b04d9a1dbc4f8bb606dfc6ba5b858fe8fd2b9
Pedro P <peucarneiro@hotmail.com>
2017-08-04 18:38:16 -0300

    Player e movimentação básica adicionados

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    public float speed; //Velocidade do player
+
+    private Rigidbody2D rigid; //Componente Rigidbody2D do player
+
+	void Start () {
+
+        rigid = gameObject.GetComponent<Rigidbody2D>(); 
+
+	}
+	
+	void Update () {
+
+        rigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+		
+	}
+}
