792531c55d806e86bf3b1dc5c99579583376b5e4
JR-PC\JR <jr.lsf26@gmail.com>
2017-08-05 14:23:17 -0300

    Adicionado o Script PlayerHealth que contabiliza e gerencia a vida do player

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float health;
+	public float dmgAmountTest;
+	public Text healthDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		healthDisplay.text = "Health:" + health;
+
+
+		///if (Input.GetKeyDown (KeyCode.T)) {
+		//	health -= dmgAmountTest;
+		//}
+
+		if (health <= 0) {
+			Destroy (this.gameObject);
+		}
+
+	}
+}
