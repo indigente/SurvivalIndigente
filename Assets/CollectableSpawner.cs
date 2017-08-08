c169414b484257735b3803d03f45b608813569c3
JR-PC\JR <jr.lsf26@gmail.com>
2017-08-08 20:51:41 -0300

    Por enquanto estou tratando a dimensão da tela atual pelos valores de X e Y atuais (X indo de -12 a 12 e Y indo de -4.5 a 4.5). Como ainda não foi definido exatamente as condições de spawn dos coletáveis, deixei como temporariamente o botão P para spawna-los na tela. Player adicionado como prefab. Criei prefabs individuais para cada cor de coletável.

---
+++ Assets/CollectableSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpawner : MonoBehaviour {
+
+	public GameObject[] collectablesPrefabs;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			Instantiate (collectablesPrefabs [Random.Range (0, collectablesPrefabs.Length)], new Vector2 (Random.Range (-12, 12), Random.Range (-4.5f, 4.5f)), Quaternion.identity); 
+		}
+		
+	}
+}
