5b287cae56257b9d5cc3177cc5dd6f821aa4e42e
JR-PC\JR <jr.lsf26@gmail.com>
2017-08-17 18:41:46 -0300

    Sistema que alterna entre dia e noite acrescentado

---
+++ Assets/DaytimeManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DaytimeManagerScript : MonoBehaviour {
+
+	public Text curDaytime;
+	public float timer, timerAux;
+	public bool isNight;
+
+
+	// Use this for initialization
+	void Start () {
+
+		isNight = false;
+		timer = timerAux;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isNight == false) {
+			curDaytime.text = "Night";
+		} else {
+			curDaytime.text = "Day";
+		}
+
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			isNight = !isNight;
+			timer = timerAux;
+		}
+	}
+}
